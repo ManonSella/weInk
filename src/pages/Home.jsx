c90704fff9ab0d70b9092d25213b6b2f3ed5f911
Manon Sella <manonsella@gmail.com>
2024-09-14 17:40:30 +0200

    generates a random dog picture on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export default function Home() {
-  return <div className="home">Hello from home</div>;
+  const [imageUrl, setImageUrl] = useState();
+  const dogApi = "https://dog.ceo/api/breeds/image/random";
+
+  if (!imageUrl) {
+    fetch(dogApi)
+      .then((response) => response.json())
+      .then((json) => setImageUrl(json.message));
+  }
+
+  return (
+    <div className="home">
+      Hello from home <img src={imageUrl} />
+    </div>
+  );
 }
