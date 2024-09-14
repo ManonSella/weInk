b7b250fd6f3ae0aa8648a650cb44e711aff298da
Manon Sella <manonsella@gmail.com>
2024-09-14 17:00:35 +0200

    creates navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
       <div className="header">
-        <h1>Header</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
       <Outlet />
       <div className="footer">Footer</div>
