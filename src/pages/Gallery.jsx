efbac645dbdfa44cae122eef28875b5a2bc05dc4
Manon Sella <manonsella@gmail.com>
2024-09-14 17:42:53 +0200

    generates multiple random dog pictures on the gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
 export default function Gallery() {
-  return <div className="gallery">Hi from gallery</div>;
+  const [imageUrls, setImageUrls] = useState([]);
+  const multipleDogPicsUrl = "https://dog.ceo/api/breeds/image/random/9";
+
+  if (imageUrls.length === 0) {
+    fetch(multipleDogPicsUrl)
+      .then((response) => response.json())
+      .then((json) => setImageUrls(json.message));
+  }
+
+  return (
+    <div className="gallery">
+      Hello from gallery
+      {imageUrls.map((imageUrl, index) => (
+        <img key={index} src={imageUrl} />
+      ))}
+    </div>
+  );
 }
