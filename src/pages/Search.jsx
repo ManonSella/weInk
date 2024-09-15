1e0f6a696c2a7d2b1a1c602852c139f4f2d5c33d
Manon Sella <manonsella@gmail.com>
2024-09-15 14:21:22 +0200

    creates search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+
 export default function Search() {
-  return <div className="search">Search bar</div>;
+  const [dogBreed, setDogBreed] = useState("");
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const apiUrlByBreed = `https://dog.ceo/api/breed/${dogBreed}/images/random/10`;
+    fetch(apiUrlByBreed)
+      .then((response) => response.json())
+      .then((json) => setImageUrls(json.message));
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="search-dog-breed">Search images by dog breed: </label>
+        <input
+          id="search-dog-breed"
+          type="search"
+          value={dogBreed}
+          onChange={(event) => setDogBreed(event.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {imageUrls.map((imageUrl, index) => (
+        <img key={index} src={imageUrl} />
+      ))}
+    </>
+  );
 }
