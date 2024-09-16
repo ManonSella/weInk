336b0c43625fbac7b94d0d7059d3e7c94dbe60d0
Manon Sella <manonsella@gmail.com>
2024-09-16 20:32:46 +0200

    adds text content to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+/* All images were retrieved from this API: https://dog.ceo/dog-api/documentation/ */
+
 export default function Home() {
   const [imageUrl, setImageUrl] = useState();
   const dogApi = "https://dog.ceo/api/breeds/image/random";
@@ -11,8 +13,19 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="home">
-      Hello from home <img src={imageUrl} />
-    </div>
+    <>
+      <div className="home">
+        <h1>Hello and welcome to the kingdom of dogs</h1>
+        <p>
+          With hundreds of lovely dog breeds to choose from, finding the right
+          one for you can be a little bit daunting. We have you covered! Here is
+          a list of dog breeds to show you which type of dog could be your
+          perfect pet! Browse your favorite dog breeds below and find your
+          potential new canine friend!
+          <em>Please, consider adopting from a et shelter around you.</em>
+        </p>
+        <img src={imageUrl} />
+      </div>
+    </>
   );
 }
