6cbdfa111c92cd3a7288a75876e115cde52d5321
Manon Sella <manonsella@gmail.com>
2024-09-17 16:43:39 +0200

    fixes icons positioning in the nav bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,9 @@
+/* creation of the nav bar in Header with links to specific routes for each page */
 import { Link } from "react-router-dom";
 import { IoHomeOutline } from "react-icons/io5";
 import { TfiGallery } from "react-icons/tfi";
 import { IoSearchOutline } from "react-icons/io5";
-
 /* icons imported from https://react-icons.github.io/react-icons/ */
-/* creation of the nav bar in Header with links to specific routes for each page */
 
 export default function Header() {
   return (
@@ -12,17 +11,17 @@ export default function Header() {
       <nav className="max-w-[1024px] p-8 mx-auto flex">
         <ul className="flex gap-8 text-lg">
           <li>
-            <Link className="align-middle" to="/">
+            <Link className="flex items-center gap-2" to="/">
               <IoHomeOutline className="inline-block" /> Home
             </Link>
           </li>
           <li>
-            <Link className="align-middle" to="/gallery">
+            <Link className="flex items-center gap-2" to="/gallery">
               <TfiGallery className="inline-block" /> Gallery
             </Link>
           </li>
           <li>
-            <Link className="align-middle" to="/search">
+            <Link className="flex items-center gap-2" to="/search">
               <IoSearchOutline className="inline-block" /> Search
             </Link>
           </li>
